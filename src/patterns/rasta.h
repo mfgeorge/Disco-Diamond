8e91784ac2fc9058226ad9d36fd06e0cdd2254e5
ipachev <ipachev@gmail.com>
2019-05-08 22:15:22 -0700

    Fix bug in rasta

--- src/patterns/rasta.h
+++ src/patterns/rasta.h
@@ -9,28 +9,22 @@ class Rasta : public Pattern {
       virtual void init(PatternFrame* pattern_frame) override {
          auto dimension = pattern_frame->AddDimension();
          range1_ = dimension->AddLayer();
-         range2_ = dimension->AddLayer();
-         range3_ = dimension->AddLayer();
 
-         *range1_ = LinearRange(
-             0, RANGE_MAX / 3,
-             [this](uint16_t curr, uint16_t total) -> Pixel { return GREEN; });
-         *range2_ = LinearRange(
-             RANGE_MAX / 3, RANGE_MAX * 2 / 3,
-             [this](uint16_t curr, uint16_t total) -> Pixel { return YELLOW; });
-         *range3_ = LinearRange(
-             RANGE_MAX * 2 / 3, RANGE_MAX,
-             [this](uint16_t curr, uint16_t total) -> Pixel { return RED; });
-         // range_->EnableGammaCorrection();
+         *range1_ = LinearRange(0, RANGE_MAX,
+                                [this](uint16_t curr, uint16_t total) -> Pixel {
+                                   if (curr < total / 3) {
+                                      return GREEN;
+                                   }
+                                   if (curr < total * 2 / 3) {
+                                      return YELLOW;
+                                   }
+                                   return RED;
+                                });
          range1_->Wrap();
-         range2_->Wrap();
-         range3_->Wrap();
       }
 
       void step() override {
          range1_->Slide(3);
-         range2_->Slide(3);
-         range3_->Slide(3);
       }
 
       uint16_t frame_delay() override {
@@ -39,6 +33,4 @@ class Rasta : public Pattern {
 
     private:
       LinearRange* range1_;
-      LinearRange* range2_;
-      LinearRange* range3_;
 };
