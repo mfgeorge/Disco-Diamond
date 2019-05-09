854fde5c0137aa87605fc1fb174b3add6b1bc5a6
ipachev <ipachev@gmail.com>
2019-05-08 19:29:29 -0700

    Add color picker

---
+++ src/color_picker.h
@@ -0,0 +1,39 @@
+#ifndef COLOR_PICKER_H
+#define COLOR_PICKER_H
+
+#include "ledstrip.h"
+#include "color_util.h"
+
+struct Complement {
+   Pixel colors[2];
+};
+
+Complement MakeComplement(int hue) {
+   uint16_t h1 = HueInDegrees(hue);
+   uint16_t h2 = HueInDegrees((hue + 180) % 360))
+   return {HSVToRGB(h1), HSVToRGB(h2)};
+
+struct Triad {
+   Pixel colors[3];
+};
+
+Triad MakeTriad(int hue) {
+   uint16_t h1 = HueInDegrees(hue);
+   uint16_t h2 = HueInDegrees((hue + 120) % 360);
+   uint16_t h3 = HueInDegrees((hue + 240) % 360);
+   return {HSVToRGB(h1), HSVToRGB(h2), HSVToRGB(h3)};
+}
+
+struct Square {
+   Pixel colors[4];
+};
+
+Square MakeSquare(int hue) {
+   uint16_t h1 = HueInDegrees(hue);
+   uint16_t h2 = HueInDegrees((hue + 90) % 360);
+   uint16_t h3 = HueInDegrees((hue + 180) % 360);
+   uint16_t h4 = HueInDegrees((hue + 270) % 360);
+   return {HSVToRGB(h1), HSVToRGB(h2), HSVToRGB(h3), HSVToRGB(h4)};
+}
+
+#endif
